efa6d4f2dfc3ec1e648203ebfca9e5d51a273bb9
Khanh Nguyen <khanhnguyen02311@gmail.com>
2023-10-29 14:34:52 +0700

    Add testing websocket & edit staging config

--- components/API/chat/message.py
+++ components/API/chat/message.py
@@ -1,28 +1,26 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, HTTPException, WebSocketException, status
 
-# from components.functions.connection import ConnectionManager
-# from components.functions.security import handle_get_current_accountinfo
+from components.functions.connection import ConnectionManager
+from components.functions.security import handle_get_current_accountinfo
 
-router = APIRouter(prefix="/message")
+router = APIRouter()
+manager = ConnectionManager()
 
-# manager = ConnectionManager()
-#
-# @router.websocket("/connect")
-# async def chat_endpoint(websocket: WebSocket, token: str):
-#     try:
-#         account = handle_get_current_user(token)
-#     except HTTPException as e:
-#         print("Account not validated:", e.detail)
-#         raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION, reason=e.detail)
-#     # if self.active_connections.get(account.id) is not None: # implement later
-#     print(account.id)
-#
-#     await manager.connect(websocket)
-#     await manager.send_personal_message(f"{account.username} connect successfully.", websocket)
-#     try:
-#         while True:
-#             data = await websocket.receive_json()
-#             await manager.read_message(account.id, data, websocket)
-#
-#     except WebSocketDisconnect:
-#         manager.disconnect(websocket)
+
+@router.websocket("/connect")
+async def chat_endpoint(websocket: WebSocket, token: str):
+    try:
+        accountinfo = handle_get_current_accountinfo(token)
+    except HTTPException as e:
+        raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION, reason=e.detail)
+    # if self.active_connections.get(account.id) is not None: # implement later
+
+    await manager.connect(websocket)
+    # await manager.send_personal_message("Account connect successfully.", websocket)
+    try:
+        while True:
+            data = await websocket.receive_json()
+            await manager.read_message(accountinfo, data, websocket)
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
--- components/functions/connection.py
+++ components/functions/connection.py
@@ -1,36 +1,70 @@
-# from fastapi import WebSocket
-# from components.functions.chat import handle_add_message
+from datetime import datetime
+import uuid
+from enum import Enum
+
+from fastapi import WebSocket
+from pydantic import BaseModel, constr, conint
+from components.storages.models import scylla_models as s_models, postgres_models as p_models
+from components.storages.schemas import scylla_schemas as s_schemas, postgres_schemas as p_schemas
+from components.functions.group import handle_check_joined_participant
+
+
+# class MessageAction(str, Enum):
+#     message_new = "msg_new"
+#     message_del = "msg_del"
+#     message_pin = "msg_pin"
 #
 #
-# class ConnectionManager:
-#     def __init__(self):
-#         self.active_connections = []
-#
-#     async def connect(self, websocket: WebSocket):
-#         await websocket.accept()
-#         self.active_connections.append(websocket)
-#
-#     def disconnect(self, websocket: WebSocket):
-#         self.active_connections.remove(websocket)
-#
-#     async def read_message(self, account_id, message: dict, websocket: WebSocket):
-#         try:
-#             type = message['type']  # for later if needed
-#             action = message['action']
-#             group_id = message['group']
-#             content = message['content']
-#             if action == "new":
-#                 created_message = handle_add_message(account_id, group_id, content)
-#                 if created_message is None:
-#                     raise Exception("Invalid group")
-#                 await self.send_personal_message(f"SUCCESSFUL: {content}", websocket)
-#
-#         except Exception as e:
-#             await self.send_personal_message(f"UNSUCCESSFUL: {str(e)}", websocket)
-#
-#     async def send_personal_message(self, message: str, websocket: WebSocket):
-#         await websocket.send_text(message)
-#
-#     async def broadcast(self, message: str):
-#         for connection in self.active_connections:
-#             await connection.send_text(message)
+# class ConnectionMessage(BaseModel):
+#     message_from: str
+
+
+def handle_add_group_message(group_id: uuid.UUID,
+                             accountinfo_id: int,
+                             content: str,
+                             accountinfo_name: str | None,
+                             group_name: str | None,
+                             message_type: str = s_models.CONSTANT.Message_type[0]):
+    """Add group message for provided account and group. Return  \n
+    Return: (error, new_message)"""
+
+    existed, participant = handle_check_joined_participant(group_id, accountinfo_id)
+    if existed:
+        time = datetime.utcnow()
+        new_message = s_schemas.MessagePOST(content=content,
+                                            group_id=group_id,
+                                            accountinfo_id=accountinfo_id,
+                                            type=message_type,
+                                            group_name=group_name,
+                                            accountinfo_name=accountinfo_name,
+                                            time_created=time)
+        s_models.MessageByAccount.create(**new_message.model_dump())
+        new_message_by_group = s_models.MessageByGroup.create(**new_message.model_dump())
+        return None, new_message_by_group
+    return "Account not in group", None
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def read_message(self, accountinfo_id: p_models.Accountinfo, message: dict, websocket: WebSocket):
+        try:
+            await self.send_personal_message(f"SUCCESSFUL", websocket)
+
+        except Exception as e:
+            await self.send_personal_message(f"UNSUCCESSFUL: {str(e)}", websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
