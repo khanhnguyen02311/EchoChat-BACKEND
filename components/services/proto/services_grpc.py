ca6ff4a13fb62597c3a5917392212a158dae972f
Khanh Nguyen <khanhnguyen02311@gmail.com>
2023-12-13 13:29:35 +0700

    fix: remove redundant grpc endpoints

--- components/services/proto/services_grpc.py
+++ components/services/proto/services_grpc.py
@@ -16,13 +16,13 @@ class BEServicerGRPC(EchoChat_pb2_grpc.EchoChatBEServicer):
         except HTTPException as e:
             return EchoChat_pb2.AccountinfoValue(id=-1, name=e.detail, identifier=0)
 
-    def NewMessage(self, request, context):
-        input_message = s_schemas.MessagePOST(group_id=request.group_id,
-                                              accountinfo_id=request.accountinfo_id,
-                                              accountinfo_name=request.accountinfo_name,
-                                              content=request.content,
-                                              type=request.type)
-        err, new_message = handle_add_new_message(input_message)
-        if err is not None:
-            return EchoChat_pb2.NewMessageResult(result=False, error=err)
-        return EchoChat_pb2.NewMessageResult(result=True)
+    # def NewMessage(self, request, context):
+    #     input_message = s_schemas.MessagePOST(group_id=request.group_id,
+    #                                           accountinfo_id=request.accountinfo_id,
+    #                                           accountinfo_name=request.accountinfo_name,
+    #                                           content=request.content,
+    #                                           type=request.type)
+    #     err, new_message = handle_add_new_message(input_message)
+    #     if err is not None:
+    #         return EchoChat_pb2.NewMessageResult(result=False, error=err)
+    #     return EchoChat_pb2.NewMessageResult(result=True)
