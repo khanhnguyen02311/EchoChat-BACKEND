0ef07520cfb6708f8544f25fb6e5a7d7763abba9
Khanh Nguyen <khanhnguyen02311@gmail.com>
2024-01-02 11:44:59 +0700

    fix: add delay to testdata generation

--- components/services/rabbitmq/services_rabbitmq.py
+++ components/services/rabbitmq/services_rabbitmq.py
@@ -47,7 +47,9 @@ from configurations.conf import Proto
 
 
 class BEServicerRMQ(object):
-    # old version missed heartbeat -> connection closed after 60s
+    SLEEP_INTERVAL = 1
+
+    # last version missed heartbeat -> connection closed after 60s
     # https://stackoverflow.com/questions/70889479/how-to-use-pika-with-fastapis-asyncio-loop
     def __init__(self):
         self._credentials = pika.PlainCredentials(Proto.RMQ_USR, Proto.RMQ_PWD)
@@ -130,3 +132,123 @@ class BEServicerRMQ(object):
 
 
 RabbitMQService = BEServicerRMQ()
+
+# class BEServicerRMQ(object):
+#     # Last version have some problems with long operations that shut the service off in the middle
+#     PUBLIC_INTERVAL = 0.1
+#
+#     def __init__(self):
+#         self._credentials = pika.PlainCredentials(Proto.RMQ_USR, Proto.RMQ_PWD)
+#         self._parameters = pika.ConnectionParameters(Proto.RMQ_HOST, Proto.RMQ_PORT, "/", self._credentials, heartbeat=0)
+#         self._properties = pika.BasicProperties(content_type='application/json')
+#         self._connection = None
+#         self._channel = None
+#         self._stopping = False
+#
+#     def _connect(self):
+#         self._connection = pika.SelectConnection(parameters=self._parameters,
+#                                                  on_open_callback=self._on_connection_open,
+#                                                  on_open_error_callback=self._on_connection_open_error,
+#                                                  on_close_callback=self._on_connection_closed)
+#
+#     def _on_connection_open(self, unused_connection):
+#         self._connection.channel(on_open_callback=self._on_channel_open)
+#
+#     def _on_connection_closed(self, connection, reply_code, reply_text):
+#         """This method is invoked by pika when the connection to RabbitMQ is
+#         closed unexpectedly. Since it is unexpected, we will reconnect to
+#         RabbitMQ if it disconnects.
+#
+#         :param pika.connection.Connection connection: The closed connection obj
+#         :param int reply_code: The server provided reply_code if given
+#         :param str reply_text: The server provided reply_text if given
+#
+#         """
+#         print('RabbitMQ: Connection closed, reopening in 2 seconds: (%s) %s', reply_code, reply_text)
+#         self._channel = None
+#         if self._stopping:
+#             self._connection.ioloop.stop()
+#         else:
+#             self._connection.add_timeout(2, self._connection.ioloop.stop)
+#
+#     def _on_connection_open_error(self, unused_connection, err):
+#         print("RabbitMQ: Connection open failed, reopening in 2 seconds:", type(err), err)
+#         self._connection.ioloop.call_later(2, self._connection.ioloop.stop)
+#
+#     def _on_channel_open(self, channel):
+#         """This method is invoked by pika when the channel has been opened.
+#         The channel object is passed in, so we can make use of it.
+#
+#         Since the channel is now open, we'll declare the exchange to use.
+#
+#         :param pika.channel.Channel channel: The channel object
+#
+#         """
+#         self._channel = channel
+#         self._channel.add_on_close_callback(self._on_channel_closed)
+#         self._setup_exchange(Proto.RMQ_EXCHANGE)
+#
+#     def _on_channel_closed(self, channel, reason):
+#         print("RabbitMQ: Channel closed:", type(reason), reason)
+#         # self._channel = None
+#         # self.stop()
+#         if not self._stopping:
+#             self._connect()
+#         else:
+#             self._connection.close()
+#
+#     def _setup_exchange(self, exchange_name):
+#         # print("setup exchange " + exchange_name)
+#         self._channel.exchange_declare(exchange=exchange_name, exchange_type="direct", durable=True, callback=self._on_exchange_declare_ok)
+#
+#     def _on_exchange_declare_ok(self, _unused_frame):
+#         # pass through a pair of queue and corresponding routing key
+#         cb_with_value_noti = functools.partial(self._on_queue_declare_ok, queue_route=[Proto.RMQ_QUEUE_NOTI, Proto.RMQ_ROUTING_KEY_NOTI])
+#         cb_with_value_msg = functools.partial(self._on_queue_declare_ok, queue_route=[Proto.RMQ_QUEUE_MSG, Proto.RMQ_ROUTING_KEY_MSG])
+#         self._channel.queue_declare(queue=Proto.RMQ_QUEUE_NOTI,
+#                                     durable=True, exclusive=False, auto_delete=False,
+#                                     callback=cb_with_value_noti)
+#         self._channel.queue_declare(queue=Proto.RMQ_QUEUE_MSG,
+#                                     durable=True, exclusive=False, auto_delete=False,
+#                                     callback=cb_with_value_msg)
+#
+#     def _on_queue_declare_ok(self, _unused_frame, queue_route):
+#         self._channel.queue_bind(queue_route[0], Proto.RMQ_EXCHANGE, routing_key=queue_route[1])
+#
+#     def _schedule_next_message(self):
+#         """If we are not closing our connection to RabbitMQ, schedule another
+#         message to be delivered in PUBLISH_INTERVAL seconds.
+#
+#         """
+#         self._connection.add_timeout(self.PUBLIC_INTERVAL,
+#                                      self.send_data)
+#
+#     def run(self):
+#         print("RabbitMQ producer is starting...")
+#         self._connection = None
+#         try:
+#             self._connect()
+#             self._connection.ioloop.start()
+#         except KeyboardInterrupt:
+#             self.stop()
+#             if self._connection is not None and not self._connection.is_closed:
+#                 # Finish closing
+#                 self._connection.ioloop.start()
+#
+#     def stop(self):
+#         print("RabbitMQ producer is stopping...")
+#         self._stopping = True
+#         if self._channel is not None and self._channel.is_open:
+#             self._channel.close()
+#         if self._connection is not None and self._connection.is_open:
+#             self._connection.close()
+#
+#     def send_data(self, routing, data):
+#         if self._connection is None or not self._connection.is_open:
+#             print("RabbitMQ: Send data failed. Connection is not open.")
+#             return
+#         if self._channel is None or not self._channel.is_open:
+#             print("RabbitMQ: Send data failed. Channel is not open.")
+#             return
+#         self._channel.basic_publish(exchange=Proto.RMQ_EXCHANGE, routing_key=routing, body=data, properties=self._properties)
+#         self._schedule_next_message()
